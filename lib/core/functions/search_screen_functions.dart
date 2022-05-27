a956933aee64f35629665773c511585a0cc997b6
Ronald Erosa <thecasualdev@gmail.com>
2022-05-27 15:29:05 +0800

    added reloading latest news when search string is empty

--- lib/core/functions/search_screen_functions.dart
+++ lib/core/functions/search_screen_functions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nyt_articles/config/route_paths.dart';
+import 'package:nyt_articles/core/providers/data_providers.dart';
 import 'package:nyt_articles/core/providers/general_providers.dart';
 import 'package:nyt_articles/utils/registry.dart';
 import 'package:nyt_articles/utils/services/navigation_service.dart';
@@ -10,6 +11,9 @@ class SearchScreenFunctions{
     final NavigationService nav = registry<NavigationService>();
     FocusManager.instance.primaryFocus?.unfocus();
     ref.read(searchKeywordProvider.state).update((state) => data);
+    if(data.isEmpty){
+      ref.refresh(searchedArticleFutureProvider);
+    }
     nav.to(context, RoutePaths.searchedArticleViewScreen);
   }
 }
