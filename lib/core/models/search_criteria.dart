8d58d8ba828596479c3db8c740a0f5291e2e6af8
Ronald Erosa <thecasualdev@gmail.com>
2022-05-27 11:33:53 +0800

    Added Search and Most Popular Article  List View

--- lib/core/models/search_criteria.dart
+++
@@ -1,8 +0,0 @@
-import 'package:nyt_articles/constants/enums.dart';
-
-class SearchCriteria{
-  final String searchKeyword;
-  final Sorting sorting;
-
-  const SearchCriteria({required this.searchKeyword, this.sorting = Sorting.relevance});
-}
